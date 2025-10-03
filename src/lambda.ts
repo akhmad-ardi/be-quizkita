7968a14320ceb93abc9cb3876fec9cf98ca90004
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-10-03 17:36:45 +0800

    chore:delete lambda file

--- src/lambda.ts
+++
@@ -1,6 +0,0 @@
-import serverlessExpress from '@vendia/serverless-express';
-import { App } from './app';
-
-const app = new App(3000).getExpressApp();
-
-export const handler = serverlessExpress({ app });
