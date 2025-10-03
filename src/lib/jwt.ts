b6cae9af77a54b6f5a20e212ac85b0d1df6e91da
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-10-03 23:09:55 +0800

    fix:lib jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -2,8 +2,8 @@ import { SignJWT, jwtVerify } from 'jose';
 
 export const secretToken = new TextEncoder().encode(process.env.TOKEN_KEY!);
 
-export function generateToken(id: string, username: string) {
-  return new SignJWT({ id, username })
+export async function generateToken(id: string, username: string) {
+  return await new SignJWT({ id, username })
     .setProtectedHeader({ alg: 'HS256' })
     .setExpirationTime(process.env.TOKEN_AGE || '3d')
     .sign(secretToken);
